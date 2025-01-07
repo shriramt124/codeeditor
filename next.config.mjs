c5b1f5d5f06a43068f71ad2b76c26832f95ed512
Shriram Tiwari <shriramt.124@gmail.com>
2025-01-07 19:47:45 +0530

    netlify toml added

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@ import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin';
 
 export default {
     webpack(config, options) {
+        // Disable the Named Chunk feature to avoid conflicts with NFT files
+        config.output.filename = '[name].js';
+
+        // Add MonacoWebpackPlugin to the plugins array
         config.plugins.push(new MonacoWebpackPlugin());
+
         return config;
     },
 };
