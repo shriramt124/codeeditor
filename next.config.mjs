dfdb4327bede53083efe6b7aa4150752fa90393f
Shriram Tiwari <shriramt.124@gmail.com>
2025-01-07 20:45:02 +0530

    updation

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,8 @@ import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin';
 
 export default {
     webpack(config, options) {
-        // Ensure unique filenames for all assets
-        config.output.filename = '[name].[contenthash].js';
-
-        // Add MonacoWebpackPlugin to the plugins array
-        config.plugins.push(new MonacoWebpackPlugin({
-            languages: ['javascript', 'typescript', 'html', 'css'], // Add necessary languages
-            features: ['!copyPaste', '!find'], // Optional: Remove unneeded Monaco features to reduce the bundle size
-        }));
+        // Ensure unique filenames for all assets to avoid conflicts
+        config.output.filename = '[name].[contenthash].js'; // Add contenthash to avoid conflicts
 
         // Optional: Set unique chunk names to avoid conflicts in chunk names
         config.optimization.splitChunks = {
@@ -20,15 +14,15 @@ export default {
                     name: 'vendors',
                     chunks: 'all',
                 },
-                // You can also customize other cacheGroups here based on your project needs
             },
         };
 
+        // Add MonacoWebpackPlugin to the plugins array
+        //config.plugins.push(new MonacoWebpackPlugin());
+
         return config;
     },
-
-    // Disable font optimization
-    experimental: {
-        optimizeFonts: false,
+    eslint: {
+        ignoreDuringBuilds: true, // Disable ESLint during the build process
     },
 };
