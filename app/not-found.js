71207c380c8f2d7ccccbf723798ae3682422ec8f
shriram tiwari <shriramt.124@gmail.com>
2025-01-07 20:02:02 +0000

    not found page added

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
+            <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+            <p className="mt-4">The page you are looking for does not exist.</p>
+        </div>
+    );
+};
+
+export default NotFound;
